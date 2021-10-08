4198fb6e25080322090561e46a162d7817824f08
Craig Grantham <craig.grantham@claytonhomes.com>
2021-10-08 19:21:10 -0400

    First Commit with Themebau template.  Not complete, but looking pretty stable.

--- Sources/App/Controllers/WebsiteController.swift
+++ Sources/App/Controllers/WebsiteController.swift
@@ -23,7 +23,7 @@ struct WebsiteController: RouteCollection
 		routes.get("Cars", use: carsHandler)
 		routes.get("SUVs", use: suvsHandler)
 		routes.get("Trucks", use: trucksHandler)
-		routes.get("siteObjectID", ":siteObjectID", use: getSiteObjectIDHandler)
+		routes.get("siteObjectID", use: getSiteObjectIDHandler)
 		
 	}
 
@@ -31,25 +31,6 @@ struct WebsiteController: RouteCollection
 	
 	func getTopNavigation(_ req: Request) -> EventLoopFuture<[WebsiteCategory]> {
 		WebsiteCategory.query(on: req.db).sort(\.$navigationOrderNdx).all()
-	}
-
-
-	func indexHandler(_ req: Request) -> EventLoopFuture<View> {
-		
-		WebsiteCategory.query(on: req.db).sort(\.$navigationOrderNdx).all().flatMap { navItems in
-			
-			SiteObject.query(on: req.db).sort(\.$makeName).sort(\.$modelYear, .descending).all().flatMap { siteObjs in
-				
-				let siteObjects = siteObjs.isEmpty ? nil : siteObjs
-				
-				let context = IndexContext(
-					title: "WebsterMotors",
-					vehicles: siteObjects,
-					top_nav: navItems)
-				
-				return req.view.render("index", context)
-			}
-		}
 	}
 
 	
@@ -96,7 +77,47 @@ struct WebsiteController: RouteCollection
 		return SiteObject.query(on: req.db).group(.or) { group in
 			group.filter(\.$webCatogoryID == categoryID)}.sort(\.$makeName).sort(\.$modelYear, .descending).all()
 	}
+
 	
+	func indexHandler(_ req: Request) -> EventLoopFuture<View> {
+		
+		WebsiteCategory.query(on: req.db).sort(\.$navigationOrderNdx).all().flatMap { navItems in
+			
+			SiteObject.query(on: req.db).sort(\.$makeName).sort(\.$modelYear, .descending).all().flatMap { siteObjs in
+				
+				let siteObjects = siteObjs.isEmpty ? nil : siteObjs
+				
+				var pageNav: [String : Int] = [:]
+				var makeNav: [String] = []
+				var navNdx = 0
+				
+				for siteObj in siteObjs
+				{
+					if let _ = pageNav[siteObj.makeName]
+					{
+						
+					}
+					else
+					{
+						pageNav[siteObj.makeName] = navNdx
+						navNdx += 1
+						
+						makeNav.append(siteObj.makeName)
+					}
+				}
+				
+
+				let context = PageNavContext(
+					title: "WebsterMotors",
+					vehicles: siteObjects,
+					top_nav: navItems,
+					page_nav: makeNav)
+
+				return req.view.render("index", context)
+			}
+		}
+	}
+
 	
 	func carsHandler(_ req: Request) -> EventLoopFuture<View> {
 		
@@ -205,18 +226,37 @@ struct WebsiteController: RouteCollection
 	
 	func getSiteObjectIDHandler(_ req: Request) -> EventLoopFuture<View>
 	{
-		WebsiteCategory.query(on: req.db).sort(\.$navigationOrderNdx).all().flatMap { navItems in
+		guard let objectID = req
+				.query[String.self, at: "object"] else {
+					_ = Abort(.badRequest)
+					
+					return req.view.render("car-detail")
+				}
+
+		return WebsiteCategory.query(on: req.db).sort(\.$navigationOrderNdx).all().flatMap { navItems -> EventLoopFuture<View> in
 			
-			SiteObject.find(req.parameters.get("siteObjectID"), on: req.db)
-				.unwrap(or: Abort(.notFound))
-				.flatMap() { siteObject in
-					
-					let context = SiteObjectContext(
-						title: siteObject.modelName,
-						vehicle: siteObject,
-						top_nav: navItems)
-					
-					return req.view.render("detail", context)
+			SiteObject.query(on: req.db).group(.or) { or in
+				or.filter(\.$siteObjectID == objectID)
+			}.first().flatMap() { siteObject in
+				
+				var car: SiteObject!
+				
+				if let obj = siteObject
+				{
+					car = obj
+				}
+				
+				return ObjectCategoryList.query(on: req.db).group(.or) { or in
+					or.filter(\.$siteObjectID == car.siteObjectID)}.sort(\.$displayNdx).all().flatMap { carFeatures -> EventLoopFuture<View> in
+						
+						let context = SiteObjectContext(
+							title: car.modelName,
+							vehicle: car,
+							features: carFeatures,
+							top_nav: navItems)
+						
+						return req.view.render("car-detail", context)
+					}
 				}
 		}
 	}
@@ -231,10 +271,19 @@ struct IndexContext: Encodable {
 	let top_nav: [WebsiteCategory]?
 }
 
+struct PageNavContext: Encodable {
+	let title: String
+	
+	let vehicles: [SiteObject]?
+	let top_nav: [WebsiteCategory]?
+	let page_nav: [String]
+}
+
 
 struct SiteObjectContext: Encodable {
 	let title: String
 	let vehicle: SiteObject
+	let features: [ObjectCategoryList]
 	let top_nav: [WebsiteCategory]?
 }
 
